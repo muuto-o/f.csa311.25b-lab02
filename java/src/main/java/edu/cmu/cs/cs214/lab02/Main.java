54778021df1163bffb51a586837463674fc9e63d
lucicul <unmnkh@gmail.com>
2025-02-14 09:58:37 +0800

    Improved Encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(5);
+        Shape square = new Square(4);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(circle);
+        Renderer renderer3 = new Renderer(square);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
